67d895059cb9b7bf848a406456f7a88a213c3dbd
kavyagarg017 <kavyagarg017@gmail.com>
2026-03-11 09:52:16 +0530

    Added UC3 HelloApp program

--- HelloApp.java
+++ HelloApp.java
@@ -2,12 +2,12 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        String name;
+        // Default name
+        String name = "World";
 
+        // Check if a name is provided as command-line argument
         if (args.length > 0) {
-            name = args[0];
-        } else {
-            name = "World";
+            name = args[0]; // Use the provided name
         }
 
         System.out.println("Hello, " + name + "!");
