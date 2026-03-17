79d29e570b9bcc0aeb6ef18b0687a6adac333846
kavyagarg017 <kavyagarg017@gmail.com>
2026-03-17 18:50:47 +0530

    UC4 implemented

--- HelloApp.java
+++ HelloApp.java
@@ -1,15 +1,28 @@
 public class HelloApp {
 
     public static void main(String[] args) {
-
-        // Default name
-        String name = "World";
-
-        // Check if a name is provided as command-line argument
+      
         if (args.length > 0) {
-            name = args[0]; // Use the provided name
-        }
+            
+            StringBuilder nameBuilder = new StringBuilder();
+            
+            String name = ""; 
 
-        System.out.println("Hello, " + name + "!");
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+    
+                name = nameBuilder.toString();
+            }
+   
+            System.out.println("Hello, " + name + "!");
+            
+        } else {
+			
+            System.out.println("Hello, World!");
+        }
     }
 }
